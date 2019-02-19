add normalization for encode method, correct values for encode test

src/llc_spatial_pyramid_encoding.py
@@ -90,7 +90,7 @@ class LlcSpatialPyramidEncoder:
         1, followed by level 2. In case of an error, None will be returned.
         """
         # index 0: level 0 bin; 1-4: level 1 bins; 5-20: level 2 bins
-        spm_code = np.zeros(21, self._size)
+        spm_code = np.zeros((21, self._size))
         # encode all features of all level 2 bins
         for l1_bin in range(4):
             for l2_bin in range(4):
@@ -121,7 +121,20 @@ class LlcSpatialPyramidEncoder:
             for l1_bin in range(2, 5):
                 spm_code[0] += spm_code[l1_bin]
         else:
-            raise ValueError("Invalid pooling method was chosen.")
+            raise ValueError("Invalid pooling method was chosen: {}".
+                             format(pooling))
+
+        spm_code = spm_code.ravel()
+
+        # normalization
+        if normalization == 'eucl':
+            spm_code = spm_code / np.linalg.norm(spm_code)
+        elif normalization == 'sum':
+            spm_code = spm_code / np.sum(spm_code)
+        else:
+            raise ValueError("Invalid normalization method was chosen: {}".
+                             format(normalization))
+        return spm_code
 
     # todo: ravel and normalize concatenated llc vector
 
@@ -144,7 +157,8 @@ class LlcSpatialPyramidEncoder:
             for i in range(1, num_features):
                 llc_code += self._get_llc_code(features[i])
         else:
-            raise ValueError("Invalid pooling method was chosen.")
+            raise ValueError("Invalid pooling method was chosen: {}".
+                             format(pooling))
 
         return llc_code
 

src/test/llc_spatial_pyramid_encoder_test.py
@@ -95,7 +95,6 @@ class LlcSpatialPyramidEncoderTest(unittest.TestCase):
         expected = np.array([1.11015, 0.45485, 0.98056])
         self.assertArrayAlmostEqual(result, expected)
 
-    @unittest.skip("implement and test low level functions first")
     def test_encode_max_eucl(self):
         # each row is a level 2 bin, each four rows are a level 1 bin
         spatial_pyramid_features = np.array([[[[3, 0], [-1, -1]],
@@ -116,13 +115,13 @@ class LlcSpatialPyramidEncoderTest(unittest.TestCase):
                                               []]])  # l1 bin bottom right
         result = self.encoder.encode(spatial_pyramid_features, pooling='max',
                                      normalization='eucl')
-        expected = np.array([0.469204, 0.0355009, 0.152291,     # l0 bin
-                             0.174543, 0.0355009, 0.152291,     # l1 bins
+        expected = np.array([0.370860, 0.151949, 0.327569,      # l0 bin
+                             0.370860, 0.076360, 0.327569,      # l1 bins
                              0, 0, 0,
                              0, 0, 0,
-                             0.469204, -0.305246, -0.00864769,
-                             0.174543, 0.0355009, 0.152291,     # l2 bins top left
-                             0.0521114, -0.0215866, 0.124786,
+                             0.218494, 0.151949, -0.036379,
+                             0.370860, 0.076360, 0.327569,      # l2 bins top left
+                             0.116962, -0.048269, 0.265370,
                              0, 0, 0,
                              0, 0, 0,
                              0, 0, 0,                           # l2 bins top right
@@ -133,13 +132,12 @@ class LlcSpatialPyramidEncoderTest(unittest.TestCase):
                              0, 0, 0,
                              0, 0, 0,
                              0, 0, 0,
-                             0.469204, -0.305246, -0.00864769,  # l2 bins bottom right
+                             0.218494,  0.151949, -0.036379,    # l2 bins bottom right
                              0, 0, 0,
                              0, 0, 0,
                              0, 0, 0])
         self.assertArrayAlmostEqual(result, expected)
 
-    @unittest.skip("implement and test low level functions first")
     def test_encode_max_sum(self):
         # each row is a level 2 bin, each four rows are a level 1 bin
         spatial_pyramid_features = np.array([[[[3, 0], [-1, -1]],
@@ -160,24 +158,24 @@ class LlcSpatialPyramidEncoderTest(unittest.TestCase):
                                               []]])  # l1 bin bottom right
         result = self.encoder.encode(spatial_pyramid_features, pooling='max',
                                      normalization='sum')
-        expected = np.array([0.253955, 0.0192147, 0.082427,     # l0 bin
-                             0.0944705, 0.0192147, 0.082427,    # l1 bins
+        expected = np.array([0.109008, 0.04466, 0.09628,        # l0 bin
+                             0.109008, 0.02244, 0.09628,        # l1 bins
                              0, 0, 0,
                              0, 0, 0,
-                             0.253955, -0.165213, -0.00468053,
-                             0.0944705, 0.0192147, 0.082427,    # l2 bins, top left
-                             0.028205, -0.0116837, 0.0675398,
+                             0.064222, 0.044663, -0.010693,
+                             0.109008, 0.022445, 0.096283,      # l2 bins top left
+                             0.034379, -0.014188, 0.078000,
                              0, 0, 0,
                              0, 0, 0,
-                             0, 0, 0,                           # l2 bins, top right
-                             0, 0, 0,
-                             0, 0, 0,
-                             0, 0, 0,
-                             0, 0, 0,                           # l2 bins, bottom left
+                             0, 0, 0,                           # l2 bins top right
                              0, 0, 0,
                              0, 0, 0,
                              0, 0, 0,
-                             0.253955, -0.165213, -0.00468053,  # l2 bins, bottom right
+                             0, 0, 0,                           # l2 bins bottom left
+                             0, 0, 0,
+                             0, 0, 0,
+                             0, 0, 0,
+                             0.064222, 0.044663, -0.010693,     # l2 bins bottom right
                              0, 0, 0,
                              0, 0, 0,
                              0, 0, 0])