e330085fe3832e83a25514484950e404a51b49c8
rickest-rick <j.st@wwu.de>
2019-02-28 17:14:09 +0100

    arrange widgets in two column layout

--- src/camera_trap_sequencer.py
+++ src/camera_trap_sequencer.py
@@ -16,27 +16,39 @@ class CameraTrapSequencer(QtWidgets.QWidget):
         super().__init__()
 
         # create all widgets
+        self.input_type_label = QtWidgets.QLabel("Input type: ")
+
         self.directory_type = QtWidgets.QComboBox()
         self.directory_type.addItem("Database")
         self.directory_type.addItem("Directory")
+
+        self.move_method_label = QtWidgets.QLabel("Move method: ")
 
         self.move_method = QtWidgets.QComboBox()
         self.move_method.addItem("Copy")
         self.move_method.addItem("Move")
 
+        self.empty_info_label = QtWidgets.QLabel("Use empty images: ")
+
         self.empty_info = QtWidgets.QCheckBox("Empty Images")
         self.empty_info.setChecked(True)
 
-        self.input_button = QtWidgets.QPushButton("Choose Input Directory")
+        self.input_dir = QtWidgets.QLineEdit("Choose input directory")
+        self.input_dir.setReadOnly(True)
+        self.input_dir.setFixedWidth(200)
+
+        self.input_button = QtWidgets.QPushButton("Input Directory")
         self.input_button.clicked.connect(self.get_input_dir)
 
-        self.output_button = QtWidgets.QPushButton("Choose Output Directory")
+        self.output_dir = QtWidgets.QLineEdit("Choose output directory")
+        self.output_dir.setReadOnly(True)
+        self.output_dir.setFixedWidth(200)
+
+        self.output_button = QtWidgets.QPushButton("Output Directory")
         self.output_button.clicked.connect(self.get_output_dir)
 
         self.order_button = QtWidgets.QPushButton("Order Sequences")
         self.order_button.clicked.connect(self.order_sequences)
-
-        # todo: add widgets for input and output paths
 
         # inner class for horizontal lines that can be used for separation
         class VerticalSeparator(QtWidgets.QFrame):
@@ -45,14 +57,16 @@ class CameraTrapSequencer(QtWidgets.QWidget):
                 self.setFrameShape(QtWidgets.QFrame.HLine)
                 self.setFrameShadow(QtWidgets.QFrame.Sunken)
 
-        # create layout and add widgets
         self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.directory_type)
-        self.layout.addWidget(VerticalSeparator())
-        self.layout.addWidget(self.move_method)
-        self.layout.addWidget(self.empty_info)
-        self.layout.addWidget(self.input_button)
-        self.layout.addWidget(self.output_button)
+
+        # create inner layout and add widgets
+        self.inner_layout = QtWidgets.QFormLayout()
+        self.inner_layout.addRow(self.input_type_label, self.directory_type)
+        self.inner_layout.addRow(self.move_method_label, self.move_method)
+        self.inner_layout.addRow(self.empty_info_label, self.empty_info)
+        self.inner_layout.addRow(self.input_dir, self.input_button)
+        self.inner_layout.addRow(self.output_dir, self.output_button)
+        self.layout.addLayout(self.inner_layout)
         self.layout.addWidget(VerticalSeparator())
         self.layout.addWidget(self.order_button)
 
@@ -63,16 +77,16 @@ class CameraTrapSequencer(QtWidgets.QWidget):
         pass
 
     def get_input_dir(self):
-        self.get_dir("Choose Input Directory")
+        input_dir = self.get_dir("Choose Input Directory")
 
     def get_output_dir(self):
-        self.get_dir("Choose Output Directory")
+        output_dir = self.get_dir("Choose Output Directory")
 
     def get_dir(self, caption):
         dir_name = QtWidgets.QFileDialog.getExistingDirectory(self, caption,
                                                               os.path.expanduser("~"),
-                                                              option=QtWidgets.QFileDialog.ShowDirsOnly
-                                                         )
+                                                              option=QtWidgets.QFileDialog.ShowDirsOnly)
+        return dir_name
 
 
 if __name__ == '__main__':
